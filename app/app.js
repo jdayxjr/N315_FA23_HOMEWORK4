5f29d593d5617f18f3c78914c471099db27d0382
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2023-10-02 18:11:08 -0400

    .....

--- app/app.js
+++ app/app.js
@@ -1,5 +1,22 @@
-function initListeners() { }
-
 $(document).ready(function () {
-    initListeners();
+    // Display the login modal when the login button is clicked
+    $('#loginButton').click(function () {
+        $('#loginModal').css('display', 'block');
+    });
+
+    // Close the login modal when the close button is clicked
+    $('.close').click(function () {
+        $('#loginModal').css('display', 'none');
+    });
+
+    // Submit the login form
+    $('#loginForm').submit(function (e) {
+        e.preventDefault();
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        // For demonstration purposes, just show an alert indicating signed-in
+        alert('Signed in as ' + username);
+        $('#loginModal').css('display', 'none');
+    });
 });
